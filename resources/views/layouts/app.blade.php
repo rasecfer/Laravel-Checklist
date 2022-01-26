2ee45b6aae2d198cbcc1532e0ffe0054d3bd4a96
Ing. Fernando Moreno <rasecfer@gmail.com>
2022-01-26 00:00:42 -0600

    List tasks for User Checklist and Show/Hide them

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,7 @@
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.bundle.min.js"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/31.1.0/classic/ckeditor.js"></script>
 @livewireScripts
--- resources/views/users/checklists/show.blade.php
+++ resources/views/users/checklists/show.blade.php
@@ -1,23 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-lg">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                {{ $checklist->name }}
+    <div class="container-lg">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $checklist->name }}
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            @foreach($checklist->tasks as $task)
+                                <tr>
+                                    <td></td>
+                                    <td class="task-description-toggle" data-id="{{ $task->id }}">
+                                        {{$task->name}}
+                                    </td>
+                                    <td>
+                                        <svg class="icon icon-2xl" id="task-caret-bottom-{{ $task->id }}">
+                                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-caret-bottom') }}"></use>
+                                        </svg>
+                                        <svg class="icon icon-2xl d-none" id="task-caret-top-{{ $task->id }}">
+                                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-caret-top') }}"></use>
+                                        </svg>
+                                    </td>
+                                </tr>
+                                <tr class="d-none" id="task-description-{{ $task->id }}">
+                                    <td></td>
+                                    <td colspan="2">
+                                        {!! $task->description !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('scripts')
     <script>
-        ClassicEditor
-            .create( document.querySelector( '#task-textarea' ) )
-            .catch( error => {
-                console.error( error );
-            } );
+        $(function () {
+            $('.task-description-toggle').click(function () {
+                $('#task-description-' + $(this).data('id')).toggleClass('d-none');
+                $('#task-caret-bottom-' + $(this).data('id')).toggleClass('d-none');
+                $('#task-caret-top-' + $(this).data('id')).toggleClass('d-none');
+            });
+        });
     </script>
+
 @endsection
